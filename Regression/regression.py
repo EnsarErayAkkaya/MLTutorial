46ccf8861de6af8105f017c72c27011c8706e389
Ensar Eray Akkaya <33553090+EnsarErayAkkaya@users.noreply.github.com>
2020-11-20 15:32:41 +0300

    Update regression.py

--- Regression/regression.py
+++ Regression/regression.py
@@ -20,43 +20,40 @@ df['PCT_Change'] = (df['Adj_Close'] - df['Adj_Open']) / df['Adj_Open'] * 100.0 #
 df = df[[ 'Adj_Close', 'HL_PCT','PCT_Change','Adj_Volume']] # adjust data frame again for new create columns
 
 forecast_col = 'Adj_Close' # select label column
-df.fillna(-99999, inplace = True) # remove NAs
+df.fillna(-99999, inplace = True) # fill NAs
 
 forecast_out = int( math.ceil(0.01 * len(df)) ) # length of forcast  1% of data frame for now, it can change
 
 df['label'] = df[forecast_col].shift(-forecast_out) # shift Adj_Close -10 for now 
 
-
-X = np.array(df.drop(['label'], 1))# Features are X
+X = np.array(df.drop(['label'], 1)) # Features are X
 X = preprocessing.scale(X) # scale X for making it faster and easier
 X_lately = X[ -forecast_out : ] # forecast set
-X = X[ : -forecast_out : ]
-
 df.dropna(inplace = True) # clear NAs
+X = X[ : -forecast_out : ]
 
 y = np.array(df['label']) # Labels are y
 
 X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.4 ) # set train data and test data
 
-clf = LinearRegression(n_jobs=-1) # use LinearRegression, n_jobs how mony thread it will use -1 means as mush as possible
+clf = LinearRegression(n_jobs=-1) # use LinearRegression, n_jobs how many thread it will use -1 means as mush as possible
 
 clf.fit(X_train, y_train) # train the classifier
 
 accuracy =  clf.score(X_test, y_test) # test the classifier and get accuracy
 
-forecast_set = clf.predict(X_lately)
+forecast_set = clf.predict(X_lately) # predict as x_lately
 
-print(forecast_set, accuracy, forecast_out)
+print(forecast_set, accuracy, forecast_out) # print forecast
 
-df['Forecast'] = np.nan
+df['Forecast'] = np.nan # fill nan to forecast column
 
-last_date = df.iloc[-1].name
-last_unix = last_date.timestamp()
-one_day = 86400
-next_unix = last_unix + one_day
-print( datetime.datetime.fromtimestamp(next_unix) )
+last_date = df.iloc[-1].name # get last date
+last_unix = last_date.timestamp() # get unix of date
+one_day = 86400 # set day as second
+next_unix = last_unix + one_day 
 
-for i in forecast_set:
+for i in forecast_set: # Fill df with new forecast rows
     next_date = datetime.datetime.fromtimestamp(next_unix)
     next_unix += one_day
     df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
