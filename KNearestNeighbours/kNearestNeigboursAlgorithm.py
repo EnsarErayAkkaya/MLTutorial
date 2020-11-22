0af38e18b8c2ffb5820b2cc344a5dadaa50f7955
Ensar Eray Akkaya <33553090+EnsarErayAkkaya@users.noreply.github.com>
2020-11-22 15:21:58 +0300

    With self made KNearestNeighbours we are calculating breast cancer

--- KNearestNeighbours/kNearestNeigboursAlgorithm.py
+++ KNearestNeighbours/kNearestNeigboursAlgorithm.py
@@ -1,15 +1,12 @@
 import numpy as np
 from math import sqrt
-import matplotlib.pyplot as plt
 import warnings
-from matplotlib import style
 from collections import Counter
+import pandas as pd
+import random
+import os
 
-style.use('fivethirtyeight')
-
-dataset = { 'k' : [ [1,2], [2,3], [3,1] ], 'r' : [ [6,5], [7,7], [8,6] ] }
-
-new_features = [5,7]
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))) # gets current directory
 
 def k_nearest_neighbours(data, predict, k=3):
     if( len(data) >= k ):
@@ -25,9 +22,31 @@ def k_nearest_neighbours(data, predict, k=3):
 
     return vote_result
 
-result = k_nearest_neighbours(dataset, new_features, k=3)
-print(result)
+df = pd.read_csv(os.path.join(__location__, 'breast-cancer-wisconsin.txt')) # get file in directory
+df.replace('?', -99999, inplace = True) # int his dataset all empty values marked by '?' we cahnged to -99999 for making them unused
+df.drop(['id'], 1, inplace = True) # id is irrevelant for this example so we are droping it
+full_data = df.astype(float).values.tolist() # some data can be string so make it float to be certain 
+random.shuffle(full_data) # shuffle all data
 
-[ [ plt.scatter(ii[0], ii[1], s=100, c=i ) for ii in dataset[i] ]  for i in dataset ]
-plt.scatter(new_features[0],new_features[1], s = 80, c = result)
-plt.show()
+test_size = 0.2
+train_set = {2:[], 4:[]}
+test_set = {2:[], 4:[]}
+train_data = full_data[: -int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)) :]
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbours(train_set, data, k=5)
+        if vote == group :
+            correct += 1
+        total += 1
+print('Accuracy:', correct/total )
